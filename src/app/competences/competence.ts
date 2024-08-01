252d2190d7d5d150e344074363f7643b1acb5288
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-08-01 12:37:39 +0200

    competences component

---
+++ src/app/competences/competence.ts
@@ -0,0 +1,9 @@
+export class competence {
+  name: string;
+  image: string;
+
+  constructor (name: string, image: string) {
+    this.name = name;
+    this.image = image;
+  }
+}
---
+++ src/app/competences/competences-mockup.ts
@@ -0,0 +1,360 @@
+import { competence } from './competence';
+
+export const COMPETENCES: competence[] = [
+  {
+    name: 'Adobe XD',
+    image: 'svg/skills/adobe-xd.svg'
+  },
+  {
+    name: 'Adobe Audition',
+    image: 'svg/skills/adobeaudition.svg'
+  },
+  {
+    name: 'After Effects',
+    image: 'svg/skills/after-effects.svg'
+  },
+  {
+    name: 'Angular',
+    image: 'svg/skills/angular.svg'
+  },
+  {
+    name: 'AWS',
+    image: 'svg/skills/aws.svg'
+  },
+  {
+    name: 'Azure',
+    image: 'svg/skills/azure.svg'
+  },
+  {
+    name: 'Blender',
+    image: 'svg/skills/blender.svg'
+  },
+  {
+    name: 'Bootstrap',
+    image: 'svg/skills/bootstrap.svg'
+  },
+  {
+    name: 'Bulma',
+    image: 'svg/skills/bulma.svg'
+  },
+  {
+    name: 'C',
+    image: 'svg/skills/c.svg'
+  },
+  {
+    name: 'C#',
+    image: 'svg/skills/csharp.svg'
+  },
+  {
+    name: 'C++',
+    image: 'svg/skills/cplusplus.svg'
+  },
+  {
+    name: 'Canva',
+    image: 'svg/skills/canva.svg'
+  },
+  {
+    name: 'Capacitor.js',
+    image: 'svg/skills/capacitor.svg'
+  },
+  {
+    name: 'CoffeeScript',
+    image: 'svg/skills/coffeescript.svg'
+  },
+  {
+    name: 'Confluence',
+    image: 'svg/skills/confluence.svg'
+  },
+  {
+    name: 'CSS',
+    image: 'svg/skills/css.svg'
+  },
+  {
+    name: 'Dart',
+    image: 'svg/skills/dart.svg'
+  },
+  {
+    name: 'Django',
+    image: 'svg/skills/django.svg'
+  },
+  {
+    name: 'Docker',
+    image: 'svg/skills/docker.svg'
+  },
+  {
+    name: 'Express',
+    image: 'svg/skills/express.svg'
+  },
+  {
+    name: 'Fastify',
+    image: 'svg/skills/fastify.svg'
+  },
+  {
+    name: 'Figma',
+    image: 'svg/skills/figma.svg'
+  },
+  {
+    name: 'Firebase',
+    image: 'svg/skills/firebase.svg'
+  },
+  {
+    name: 'Flutter',
+    image: 'svg/skills/flutter.svg'
+  },
+  {
+    name: 'GCP',
+    image: 'svg/skills/gcp.svg'
+  },
+  {
+    name: 'GIMP',
+    image: 'svg/skills/gimp.svg'
+  },
+  {
+    name: 'Git',
+    image: 'svg/skills/git.svg'
+  },
+  {
+    name: 'GitHub',
+    image: 'svg/skills/github.svg'
+  },
+  {
+    name: 'GitLab',
+    image: 'svg/skills/gitlab.svg'
+  },
+  {
+    name: 'Go',
+    image: 'svg/skills/go.svg'
+  },
+  {
+    name: 'GraphQL',
+    image: 'svg/skills/graphql.svg'
+  },
+  {
+    name: 'Haxe',
+    image: 'svg/skills/haxe.svg'
+  },
+  {
+    name: 'HTML',
+    image: 'svg/skills/html.svg'
+  },
+  {
+    name: 'Illustrator',
+    image: 'svg/skills/illustrator.svg'
+  },
+  {
+    name: 'InDesign',
+    image: 'svg/skills/id.svg'
+  },
+  {
+    name: 'InVision',
+    image: 'svg/skills/invision.svg'
+  },
+  {
+    name: 'Ionic',
+    image: 'svg/skills/ionic.svg'
+  },
+  {
+    name: 'Java',
+    image: 'svg/skills/java.svg'
+  },
+  {
+    name: 'JavaScript',
+    image: 'svg/skills/js.svg'
+  },
+  {
+    name: 'Jira',
+    image: 'svg/skills/jira.svg'
+  },
+  {
+    name: 'Julia',
+    image: 'svg/skills/julia.svg'
+  },
+  {
+    name: 'Kotlin',
+    image: 'svg/skills/kotlin.svg'
+  },
+  {
+    name: 'Lightroom',
+    image: 'svg/skills/lightroom.svg'
+  },
+  {
+    name: 'Markdown',
+    image: 'svg/skills/markdown.svg'
+  },
+  {
+    name: 'Material-UI',
+    image: 'svg/skills/material-ui.svg'
+  },
+  {
+    name: 'Materialize',
+    image: 'svg/skills/materialize.svg'
+  },
+  {
+    name: 'Matlab',
+    image: 'svg/skills/matlab.svg'
+  },
+  {
+    name: 'MemSQL',
+    image: 'svg/skills/memsql.svg'
+  },
+  {
+    name: 'Microsoft Office',
+    image: 'svg/skills/microsoft-office.svg'
+  },
+  {
+    name: 'MongoDB',
+    image: 'svg/skills/mongoDB.svg'
+  },
+  {
+    name: 'MySQL',
+    image: 'svg/skills/mysql.svg'
+  },
+  {
+    name: 'Next.js',
+    image: 'svg/skills/nextJS.svg'
+  },
+  {
+    name: 'NGINX',
+    image: 'svg/skills/nginx.svg'
+  },
+  {
+    name: 'Node.js',
+    image: 'svg/skills/nodeJS.svg'
+  },
+  {
+    name: 'Numpy',
+    image: 'svg/skills/numpy.svg'
+  },
+  {
+    name: 'Node.js',
+    image: 'svg/skills/nodejs.svg'
+  },
+  {
+    name: 'Nuxt.js',
+    image: 'svg/skills/nuxtjs.svg'
+  },
+  {
+    name: 'OpenCV',
+    image: 'svg/skills/opencv.svg'
+  },
+  {
+    name: 'Photoshop',
+    image: 'svg/skills/photoshop.svg'
+  },
+  {
+    name: 'PHP',
+    image: 'svg/skills/php.svg'
+  },
+  {
+    name: 'PicsArt',
+    image: 'svg/skills/picsart.svg'
+  },
+  {
+    name: 'PostgreSQL',
+    image: 'svg/skills/postgresql.svg'
+  },
+  {
+    name: 'Premiere Pro',
+    image: 'svg/skills/premierepro.svg'
+  },
+  {
+    name: 'Premiere Pro',
+    image: 'svg/skills/pp.svg'
+  },
+  {
+    name: 'Python',
+    image: 'svg/skills/python.svg'
+  },
+  {
+    name: 'PyTorch',
+    image: 'svg/skills/pytorch.svg'
+  },
+  {
+    name: 'React',
+    image: 'svg/skills/react.svg'
+  },
+  {
+    name: 'Ruby',
+    image: 'svg/skills/ruby.svg'
+  },
+  {
+    name: 'Sass',
+    image: 'svg/skills/sass.svg'
+  },
+  {
+    name: 'Selenium',
+    image: 'svg/skills/selenium.svg'
+  },
+  {
+    name: 'Sketch',
+    image: 'svg/skills/sketch.svg'
+  },
+  {
+    name: 'Slack',
+    image: 'svg/skills/slack.svg'
+  },
+  {
+    name: 'SQLite',
+    image: 'svg/skills/sqlite.svg'
+  },
+  {
+    name: 'Strapi',
+    image: 'svg/skills/strapi.svg'
+  },
+  {
+    name: 'Svelte',
+    image: 'svg/skills/svelte.svg'
+  },
+  {
+    name: 'Swift',
+    image: 'svg/skills/swift.svg'
+  },
+  {
+    name: 'Tailwind CSS',
+    image: 'svg/skills/tailwindcss.svg'
+  },
+  {
+    name: 'TensorFlow',
+    image: 'svg/skills/tensorflow.svg'
+  },
+  {
+    name: 'Trello',
+    image: 'svg/skills/trello.svg'
+  },
+  {
+    name: 'TypeScript',
+    image: 'svg/skills/ts.svg'
+  },
+  {
+    name: 'Ubuntu',
+    image: 'svg/skills/ubuntu.svg'
+  },
+  {
+    name: 'Unity',
+    image: 'svg/skills/unity.svg'
+  },
+  {
+    name: 'Vite.js',
+    image: 'svg/skills/vitejs.svg'
+  },
+  {
+    name: 'Vue.js',
+    image: 'svg/skills/vuejs.svg'
+  },
+  {
+    name: 'Vuetify',
+    image: 'svg/skills/vuetifyjs.svg'
+  },
+  {
+    name: 'Webix',
+    image: 'svg/skills/webix.svg'
+  },
+  {
+    name: 'Wolfram Alpha',
+    image: 'svg/skills/wolframalpha.svg'
+  },
+  {
+    name: 'WordPress',
+    image: 'svg/skills/wordpress.svg'
+  }
+];
