74ac278cf8872c3a62b4f80c24fcb32d897a6b95
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-08-02 11:58:28 +0200

    header options

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private location: Location) {}
 
+  get languageSwitch(): { url: string, label: string, src: string } {
+    const path = this.location.path();
+    if (path.includes('/fr')) {
+      return { url: '/en', label: 'fr', src: 'svg/flags/united-kingdom.svg' };
+    } else if (path.includes('/en')) {
+      return { url: '/fr', label: 'fr', src: 'svg/flags/france.svg' };
+    }
+    return { url: '/fr', label: 'fr', src: 'svg/flags/france.svg' };  // Default values
+  }
 }
