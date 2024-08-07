8ec70d78d3ae06bcb8d58c0499cda3ce4a45b1da
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-08-07 13:22:28 +0200

    update project component

--- src/app/project/projects.ts
+++ src/app/project/projects.ts
@@ -11,11 +11,11 @@ export const PROJECTS: project[] = [
   },
   {
     language: 'fr',
-    title: 'Vintage Auto 83',
-    description: 'Site web du club de véhicules anciens Vintage Auto 83, application Angular, html, css, js et materialize css.',
+    title: 'Angular Pokedex',
+    description: "Application angular du pokédex de première génération, possibilité de recherche, d'ajout, d'edition et de suppression de pokémon.",
     image: 'png/voiture.png',
-    githubLink: 'https://github.com/Punkiepoux/vintage-auto-app',
-    webLink: 'https://google.com'
+    githubLink: 'https://github.com/Punkiepoux/angular-pokedex',
+    webLink: ''
   },
   {
     language: 'fr',
@@ -35,11 +35,11 @@ export const PROJECTS: project[] = [
   },
   {
     language: 'en',
-    title: 'Vintage Auto 83',
-    description: 'Web site of the classic car club Vintage Auto 83, Angular application, html, css, js et materialize css.',
+    title: 'Angular Pokedex',
+    description: 'Angular application of the first generation pokédex, possibility of searching, adding, editing and deleting pokémon.',
     image: 'png/voiture.png',
-    githubLink: 'https://github.com/Punkiepoux/vintage-auto-app',
-    webLink: 'https://google.com'
+    githubLink: 'https://github.com/Punkiepoux/angular-pokedex',
+    webLink: ''
   },
   {
     language: 'en',
