d5af835dcbf2087bca9fde5d9093c33f88778490
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-08-08 10:54:42 +0200

    footer update

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { LanguageService } from '../language.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
+  lg: string = '';
 
+  constructor(private languageService: LanguageService,) { }
+
+  ngAfterViewInit(): void {
+    this.lg = this.languageService.lg;
+  }
 }
